14ad59d4b8947d8281545fe902ca8a8d459bef79
Lauro Guedes <leowgweb@gmail.com>
2024-11-07 22:04:31 +0000

    chore(layout): Create first project structure

---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    <livewire:instagram-screen/>
+</x-app-layout>
---
+++ resources/views/livewire/instagram-screen.blade.php
@@ -0,0 +1,12 @@
+<?php
+
+use Livewire\Volt\Component;
+
+new class extends Component {
+    //
+}; ?>
+
+<div class="grid grid-cols-3 h-screen">
+    <div class="bg-gray-500">form</div>
+    <div class="bg-gray-700 col-span-2">view</div>
+</div>
